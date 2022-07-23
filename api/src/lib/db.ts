2a23ff2d49e559341d49a9471d5e049ba67e7c39
Jesse House <jesse.house@codingzeal.com>
2022-07-22 17:26:22 -0700

    Add more examples in seed file
    Log SQL to stdout

--- api/src/lib/db.ts
+++ api/src/lib/db.ts
@@ -1,7 +1,7 @@
 // See https://www.prisma.io/docs/reference/tools-and-interfaces/prisma-client/constructor
 // for options.
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 
 import { emitLogLevels, handlePrismaLogging } from '@redwoodjs/api/logger'
 
@@ -10,12 +10,25 @@ import { logger } from './logger'
 /*
  * Instance of the Prisma Client
  */
-export const db = new PrismaClient({
-  log: emitLogLevels(['info', 'warn', 'error']),
+export const db = new PrismaClient<
+  Prisma.PrismaClientOptions,
+  'info' | 'warn' | 'error' | 'query'
+>({
+  log: emitLogLevels(['info', 'warn', 'error', 'query']),
 })
 
 handlePrismaLogging({
   db,
   logger,
-  logLevels: ['info', 'warn', 'error'],
+  logLevels: ['info', 'warn', 'error', 'query'],
+})
+
+db.$on('query', (e: Prisma.QueryEvent) => {
+  console.log(' ')
+  console.log('----------------------------')
+  console.log('Query: ' + e.query)
+  console.log('Params: ' + e.params)
+  console.log('Duration: ' + e.duration + 'ms')
+  console.log('----------------------------')
+  console.log(' ')
 })
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,28 +1,214 @@
 import { db } from 'api/src/lib/db'
 
 export default async () => {
-  const gojira = await db.band.create({
+  // !!! this seed is not idempotent, for demonstration purposes only
+  const gojira = await seedGojira()
+  console.log(`CREATED ${gojira.name}`)
+
+  // const metallica = await seedMetallica()
+  // console.log(`CREATED ${metallica.name}`)
+
+  await runGojiraQueries(gojira.id)
+}
+
+async function seedGojira() {
+  // == create Band
+  const gojira = await db.band.create({ data: { name: 'Gojira' } })
+
+  // == create Album: belongs_to Band, Band has_many Albums
+  const magma = await db.album.create({
     data: {
-      name: 'Gojira',
-      albums: {
-        create: [
-          {
-            name: 'Magma',
-            songs: {
-              create: [
-                { name: 'The Shooting Star' },
-                { name: 'Silvera' },
-                { name: 'The Cell' },
-                { name: 'Stranded' },
-              ],
-            },
-          },
+      name: 'Magma',
+      bandId: gojira.id,
+    },
+  })
+
+  // == create Songs: belongs_to Album, Album has_many Songs
+  // side one: SongUncheckedCreateInput { ablumId: string }
+  const shootingStar = await db.song.create({
+    data: { name: 'The Shooting Star', albumId: magma.id },
+  })
+  const silvera = await db.song.create({
+    data: { name: 'Silvera', albumId: magma.id },
+  })
+  const theCell = await db.song.create({
+    data: { name: 'The Cell', albumId: magma.id },
+  })
+  const stranded = await db.song.create({
+    data: { name: 'Stranded', albumId: magma.id },
+  })
+  await db.song.create({ data: { name: 'Yellow Stone', albumId: magma.id } })
+
+  // side two: SongCreateInput
+  // { album: AlbumCreateNestedOneWithoutSongsInput: { connect?: AlbumWhereUniqueInput } }
+  await db.song.create({
+    data: { name: 'Magma', album: { connect: { id: magma.id } } },
+  })
+  // NOTE: this compiles but fails at runtime, I _thought_ connect allowed you to pass objects?
+  // await db.song.create({
+  //   data: { name: 'Magma', album: { connect: magma } },
+  // })
+  // Invalid `db.song.create()` invocation
+  // Argument data.album.connect of type AlbumWhereUniqueInput needs exactly one argument,
+  // but you provided id and name and bandId.
+  // Please choose one. Available args:
+  // type AlbumWhereUniqueInput { id?: String }
+
+  // the rest of side two songs...
+  await db.song.createMany({
+    data: [
+      { name: 'Pray', albumId: magma.id },
+      { name: 'Only Pain', albumId: magma.id },
+      { name: 'Low Lands', albumId: magma.id },
+    ],
+  })
+
+  // == create Musician
+  const joe = await db.musician.create({ data: { name: 'Joe Duplantier' } })
+
+  // assign Musician to a band: explicit many-to-many
+  await db.bandMember.create({
+    data: {
+      active: true,
+      bandId: gojira.id,
+      musicianId: joe.id,
+    },
+  })
+
+  // update the writing credits on two songs: implicit many-to-many
+  await db.song.update({
+    where: { id: theCell.id },
+    data: {
+      writingCredits: { connect: { id: joe.id } },
+    },
+  })
+  await db.song.update({
+    where: { id: stranded.id },
+    data: {
+      writingCredits: { connect: { id: joe.id } },
+    },
+  })
+
+  // create Musician
+  //   and create a new Song with writing credits for this Musician
+  //   and apply writing credits to existing songs: implicit many-to-many
+  //   and assign to a band by creating a BandMember record: explicit many-to-many
+  const mario = await db.musician.create({
+    data: {
+      name: 'Mario Duplantier',
+      songs: {
+        create: [{ name: 'Liberation', albumId: magma.id }],
+        connect: [
+          { id: theCell.id },
+          { id: silvera.id },
+          { id: shootingStar.id },
         ],
+      },
+      members: {
+        create: {
+          bandId: gojira.id,
+        },
       },
     },
   })
-  console.log(`CREATED ${gojira.name}`)
 
+  // add to writing credits
+  await db.song.update({
+    where: { id: stranded.id },
+    data: {
+      writingCredits: {
+        connect: {
+          id: mario.id,
+        },
+      },
+    },
+  })
+
+  // remove writing credit: delete implicit many-to-many
+  await db.song.update({
+    where: { id: shootingStar.id },
+    data: {
+      writingCredits: {
+        disconnect: {
+          id: mario.id,
+        },
+      },
+    },
+  })
+
+  // == create Musician
+  const frank = await db.musician.create({ data: { name: 'Frank Risso' } })
+
+  // assign Musician to the band: explicit many-to-many
+  await db.bandMember.create({
+    data: { bandId: gojira.id, musicianId: frank.id },
+  })
+  // remove from band: explicit many-to-many
+  await db.band.update({
+    where: { id: gojira.id },
+    data: {
+      members: {
+        deleteMany: {
+          musicianId: frank.id,
+        },
+      },
+    },
+  })
+
+  return gojira
+}
+
+async function runGojiraQueries(bandId) {
+  const gojira = await db.band.findUnique({ where: { id: bandId } })
+  console.log(gojira.id, gojira.name)
+
+  const gojira1 = await db.band.findFirst({ where: { name: 'Gojira' } })
+  console.log(gojira1.id, gojira1.name)
+
+  const gojira2 = await db.band.findFirst({
+    where: { name: 'Gojira' },
+    include: { albums: true },
+  })
+  console.log(gojira2.id, gojira2.name, gojira2.albums.length)
+
+  const gojira3 = await db.band.findFirst({
+    where: { name: 'Gojira' },
+    include: {
+      members: {
+        include: { musician: true },
+      },
+      albums: {
+        include: {
+          songs: {
+            include: {
+              writingCredits: true,
+            },
+          },
+        },
+      },
+    },
+  })
+  console.log(gojira3.id, gojira3.name, gojira3.albums.length)
+
+  console.log(' ')
+  gojira3.albums.forEach((album) => {
+    console.log(`  ${album.name}`)
+    album.songs.forEach((song) => {
+      console.log(`  - ${song.name}`)
+      song.writingCredits.forEach((credit) => {
+        console.log(`    * ${credit.name}`)
+      })
+    })
+  })
+
+  console.log(' ')
+  gojira3.members.forEach((member) => {
+    console.log(member.musician.name)
+  })
+}
+
+async function seedMetallica() {
+  const jason = await db.musician.create({ data: { name: 'Jason Newsted ' } })
   const metallica = await db.band.create({
     data: {
       name: 'Metallica',
@@ -69,6 +255,18 @@ export default async () => {
               create: { name: 'Lars Ulrich' },
             },
           },
+          {
+            active: false,
+            musician: {
+              connect: { id: jason.id },
+            },
+          },
+          {
+            active: false,
+            musician: {
+              create: { name: 'Ron McGovney' },
+            },
+          },
         ],
       },
     },
@@ -77,8 +275,7 @@ export default async () => {
 
   const cliff = await db.musician.create({ data: { name: 'Cliff Burton' } })
   const dave = await db.musician.create({ data: { name: 'Dave Mustaine' } })
-  const jason = await db.musician.create({ data: { name: 'Jason Newsted ' } })
-  const ron = await db.musician.create({ data: { name: 'Ron McGovney' } })
+
   await db.bandMember.create({
     data: {
       active: false,
@@ -98,13 +295,6 @@ export default async () => {
       active: false,
       bandId: metallica.id,
       musicianId: jason.id,
-    },
-  })
-  await db.bandMember.create({
-    data: {
-      active: false,
-      bandId: metallica.id,
-      musicianId: ron.id,
     },
   })
 
@@ -148,4 +338,6 @@ export default async () => {
     where: { albumId: rideTheLightning.id },
   })
   console.log(`CREATED ${rideTheLightningSongs.length} songs`)
+
+  return { name: 'TODO: Metallica' }
 }
